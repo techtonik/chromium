826e8235e0d5f5314da779d9c70b5028c9764ed3
Vincent Scheib <scheib@chromium.org>
2014-04-29 14:25:11 -0700

    WIP

---
+++ content/public/browser/service_worker_host_client.h
@@ -0,0 +1,23 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_PUBLIC_BROWSER_SERVICE_WORKER_HOST_CLIENT_H_
+#define CONTENT_PUBLIC_BROWSER_SERVICE_WORKER_HOST_CLIENT_H_
+
+#include "ipc/ipc_listener.h"
+
+namespace content {
+
+// Interface for clients of ServiceWorkerHost listening to messages from service
+// worker version farthest along the install flow, typically the current
+// version.
+class ServiceWorkerHostClient : public IPC::Listener {
+  // When the service worker being listened to changes version (to a new one,
+  // or to an unregistered state).
+  virtual void OnVersionChanged() {}
+};
+
+}  // namespace content
+
+#endif  // CONTENT_PUBLIC_BROWSER_SERVICE_WORKER_HOST_CLIENT_H_
