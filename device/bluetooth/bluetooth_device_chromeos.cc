ace46ae43536da38eaf252512389ab7fc2b2536d
Vincent Scheib <scheib@>
2015-08-28 10:28:50 -0700

    bluetooth: Add adapter to BluetoothDevice
    The base class BluetoothDevice::CreateGattConnection implementation
    requires access to the BluetoothAdapter. This patch updates all
    subclasses to provide it.
    
    Part of a 4 patch series:
    https://crrev.com/1287753002 Remove Disconnect callback
    https://crrev.com/1284073002 Add adapter_ to BluetoothDevice <<<
    https://crrev.com/1292263002 BluetoothDevice::CreateGattConnection Impl
    https://crrev.com/1256313002 Android Impl & tests.
    
    BUG=512643
    
    patch from issue 1284073002 at patchset 40001 (http://crrev.com/1284073002#ps40001)

--- device/bluetooth/bluetooth_device_chromeos.cc
+++ device/bluetooth/bluetooth_device_chromeos.cc
@@ -125,7 +125,8 @@ BluetoothDeviceChromeOS::BluetoothDeviceChromeOS(
     const dbus::ObjectPath& object_path,
     scoped_refptr<base::SequencedTaskRunner> ui_task_runner,
     scoped_refptr<device::BluetoothSocketThread> socket_thread)
-    : adapter_(adapter),
+    : BluetoothDevice(adapter),
+      adapter_(adapter),
       object_path_(object_path),
       num_connecting_calls_(0),
       connection_monitor_started_(false),
